a786b4330580d5bc6356c0b61d5d6eddcc88a402
DanielStanczak <208330@edu.p.lodz.pl>
2016-05-17 11:15:01 +0200

    dodanie skrótów

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,12 +1,36 @@
 var AWS = require("aws-sdk");
+var helpers = require("../helpers");
 
 AWS.config.loadFromPath('./config.json');
 
-var task =  function(request, callback){
-var bucket = request.query["bucket"];
-var key =request.query["key"];
+	var task =  function(request, callback){
+		
+		var params ={
+		Bucket: request.query.bucket,
+		Key: request.query.key
+		}
 	
-callback(null, "Dodano do bucket: " + bucket + " " + "za pomoca klucza: " + key);
+	var s3=new AWS.S3();
+
+
+	//callback(null, "Dodano do bucket: " + bucket + " " + "za pomoca klucza: " + key);
+
+	s3.getObject(params, function(err, data) {
+	
+		var doc = data.body;
+		var algorithms=['md5','sha1','sha256','sha512'];
+		var loop =1;
+
+			helpers.calculateMultiDigest(doc, 
+				algorithms, 
+				function(err, digests) {
+					callback(null, digests.join("<br>"));	
+				}, 
+				loopCount);
+			
+	});
+
 }
+
 
 exports.action = task
