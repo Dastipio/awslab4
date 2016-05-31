cbbd57c7aa237c7f00959c0773ed06ac4e95a8be
DanielStanczak <208330@edu.p.lodz.pl>
2016-05-31 10:36:03 +0200

    dodanie  simpleDb.js oraz createDomain

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,36 +1,46 @@
 var AWS = require("aws-sdk");
 var helpers = require("../helpers");
+var simpleDb = require("./simpleDb");
+AWS.config.loadFromPath('./config.json');
+
+var task = function(request, callback){
+	var params = {
+  Bucket: request.query.bucket,
+  Key: request.query.key
+};
+			
 
 AWS.config.loadFromPath('./config.json');
 
-	var task =  function(request, callback){
-		
-		var params ={
-		Bucket: request.query.bucket,
-		Key: request.query.key
-		}
+var s3 = new AWS.S3(); 
+
+s3.getObject(params, function(err, data) {
+	var algorithms = ['md5','sha1','sha256', 'sha512'];
+	var loopCount = 1;
+	var doc = data.Body;
 	
-	var s3=new AWS.S3();
-
-
-	//callback(null, "Dodano do bucket: " + bucket + " " + "za pomoca klucza: " + key);
-
-	s3.getObject(params, function(err, data) {
-		if(err) {callback(err); return;}
-		var doc = data.body;
-		var algorithms=['md5','sha1','sha256','sha512'];
-		var loopCount =1;
-
-			helpers.calculateMultiDigest(doc, 
-				algorithms, 
-				function(err, digests) {
-					callback(null, digests.join("<br>"));	
-				}, 
-				loopCount);
+	
+	helpers.calculateMultiDigest(doc, 
+		algorithms, 
+		function(err, digests) {
+			callback(null, digests.join("<br>"));				
+			// simpleDb.createDomain(function(){
 			
-	});
-
+		    // var AttributesPut = [ 
+				// {		
+					// Name: 'MD5', /* required */
+					// Value: 'TEST' /* required */
+				// },
+			// ];			
+			// simpleDb.putAttributes('Plik1', AttributesPut, function(){
+			   
+			   	 
+			   // });
+			// });		
+			simpleDb.getFromDb('Plik1');			
+		}, 
+		loopCount);   // successful response
+});
 }
-
 
 exports.action = task
